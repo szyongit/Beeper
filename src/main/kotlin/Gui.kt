2014a080f75052bfd24d7764f5af2ae6e91f1df6
Szyon <110719919+szyondevs@users.noreply.github.com>
2022-10-05 18:37:35 +0200

    Initial commit

---
+++ src/main/kotlin/Gui.kt
@@ -0,0 +1,44 @@
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Point
+import java.awt.event.*
+import javax.swing.JButton
+import javax.swing.JFrame
+
+private lateinit var frame: JFrame
+private lateinit var button: JButton
+
+fun load(title:String) {
+    frame = JFrame(title)
+    frame.size = Dimension(320, 300)
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE;
+
+
+    var screenSize = getToolkit().screenSize;
+    frame.location = Point(screenSize.width / 2 - frame.width / 2, screenSize.height / 2 - frame.height / 2)
+
+    button()
+    frame.add(button)
+
+    frame.addComponentListener(object : ComponentAdapter() {
+        override fun componentResized(e: ComponentEvent?) {
+            resizeButton(frame.width, frame.height)
+        }
+    })
+
+    frame.isVisible = true;
+}
+
+private fun button() {
+    button = JButton("Beep!")
+    button.background = Color.WHITE
+    button.isFocusPainted = false
+
+    button.addActionListener { getToolkit().beep() }
+}
+
+private fun resizeButton(width:Int, height:Int) {
+    if(button == null) return
+    button.size = Dimension(width, height)
+    button.font = button.font.deriveFont(height.toFloat() / 10)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import java.awt.Toolkit
+
+fun main(args:Array<String>) {
+    load("Beeper!")
+}
+
+fun getToolkit():Toolkit {
+    return Toolkit.getDefaultToolkit();
+}
